16a6f0602768067d1fb1a348283578565f02a9f5
Johann Schmitz <johann@j-schmitz.net>
2015-02-19 17:36:25 +0100

    Don't use passwords as command line arguments; use a defaults file instead. Dropped support for --all-databases - each database goes into its own file. Use a subprocess to mysql binary to get the list of databases. As a result, this script uses std library only

--- src/mysql-dbdump.py
+++ src/mysql-dbdump.py
@@ -1,142 +1,70 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from argparse import ArgumentParser
 
-import MySQLdb
 import logging
 import os
-import tarfile
 import datetime
-import re
-import sys
+import shlex
+from subprocess import Popen, PIPE
+
 
 class DatabaseDumper(object):
-	filename_pattern_single = "{YEAR}-{MONTH}-{DAY}-{DATABASE}.sql"
-	filename_pattern_all = "{YEAR}-{MONTH}-{DAY}-ALL.sql"
 
-	def __init__(self, host, user, password, output_dir, one_file_per_database, compress, dump_opts):
-		self.host = host
-		self.user = user
-		self.password = password
-		self.seperate_databases = one_file_per_database
-		self.compress = compress
+	def __init__(self, defaults_file, output_dir, format):
+		self.defaults_file = defaults_file
 		self.output_dir = output_dir
-		self.dump_opts = dump_opts
+		self.format = format
 
-	def get_db_names(self):
-		connection = None
-		cursor = None
+	def list_databases(self):
+		cmd = "mysql --defaults-file=%s --disable-column-names -b -s -e 'SHOW DATABASES;'" % self.defaults_file
+		process = Popen(shlex.split(cmd), stdout=PIPE)
+		out, _ = process.communicate()
+		out = out.decode('unicode_escape')
 
-		tables = []
-
-		try:
-			connection = MySQLdb.connect(self.host, self.user, self.password, 'mysql')
-
-			cursor = connection.cursor()
-			cursor.execute('SHOW DATABASES;')
-
-			for row in cursor.fetchall():
-				if not 'lost+found' in row[0]:
-					tables.append(row[0])
-
-			return tables
-		finally:
-			if cursor:
-				cursor.close()
-			if connection:
-				connection.close()
+		for db in (x.strip() for x in out.split('\n')):
+			if db:
+				yield db
 
 	def dump_database(self, database, outfile):
 		logging.debug("Dumping database '%s' to '%s'" % (database, outfile))
 
-		cmd = "/usr/bin/mysqldump"
-		cmdargs = "-u %s --password=%s -h %s --disable-keys --skip-add-locks --quote-names --events --databases %s %s -r %s" % (self.user, self.password, self.host, database, self.dump_opts, outfile)
+		cmd = "mysqldump --defaults-file=%s --disable-keys --skip-add-locks --quote-names --events --databases %s" % (self.defaults_file, database)
 
-		os.system("%s %s" % (cmd, cmdargs))
-
-	def dump_all_databases(self, outfile):
-		logging.debug("Dumping all databases '%s'" % outfile)
-
-		cmd = "/usr/bin/mysqldump"
-		cmdargs = "-u %s --password=%s -h %s --disable-keys --skip-add-locks --quote-names --events -A %s -r %s" % (self.user, self.password, self.host, self.dump_opts, outfile)
-
-		os.system("%s %s" % (cmd, cmdargs))
-
-	def compress_file(self, infile, outfile):
-		out = None
-		try:
-			out = tarfile.TarFile.open(outfile, 'w:bz2')
-
-			out.add(infile, arcname=os.path.basename(infile))
-
-		finally:
-			if out:
-				out.close()
-
-	def get_output_filename(self, filename, additional_replacements=None):
-		file = os.path.join(self.output_dir, filename)
-
-		now = datetime.datetime.now()
-
-		# replace all markers in the filename pattern
-		file = re.compile('{YEAR}', re.IGNORECASE).sub(now.strftime('%Y'), file)
-		file = re.compile('{MONTH}', re.IGNORECASE).sub(now.strftime('%m'), file)
-		file = re.compile('{DAY}', re.IGNORECASE).sub(now.strftime('%d'), file)
-
-		if additional_replacements:
-			# additional_replacements are used f.e. in the db task
-			for search, repl in additional_replacements.iteritems():
-				file = re.compile(search, re.IGNORECASE).sub(repl, file)
-
-		return file
-
-	def create_dir_if_not_exists(self, dir):
-		if not os.path.exists(dir) or not os.path.isdir(dir):
-			os.mkdir(dir)
+		with open(outfile, 'wb') as o:
+			dump_process = Popen(shlex.split(cmd), stdout=PIPE)
+			gzip_process = Popen(shlex.split("gzip -c - "), stdin=dump_process.stdout, stdout=o)
+			dump_process.stdout.close()
+			dump_process.wait()
+			gzip_process.wait()
 
 	def execute(self):
-		databases = None
+		if not os.path.exists(self.output_dir):
+			os.makedirs(self.output_dir, 0o700)
 
-		# get a list of all databases if seperate_databases = True
-		if self.seperate_databases:
-			databases = self.get_db_names()
+		now = datetime.datetime.now()
+		for database in self.list_databases():
+			if database in ('lost+found', 'information_schema', 'performance_schema'):
+				continue
 
-		outfiles = []
+			fargs = dict((x, getattr(now, x)) for x in ('day', 'month', 'year', 'hour', 'minute', 'second'))
+			fargs['name'] = database
+			filename = self.format % fargs
 
-		if databases:
-			for db in databases:
-				if db != "information_schema" and db != "performance_schema":
-					# dump the current database
-					out = self.get_output_filename(self.filename_pattern_single, { '{DATABASE}': db })
-					self.create_dir_if_not_exists(os.path.dirname(out))
-					outfiles.append(out)
-					self.dump_database(db, out)
-		else:
-			# otherwise, put all databases in one big file
-			out = self.get_output_filename(self.filename_pattern_all)
-			self.create_dir_if_not_exists(os.path.dirname(out))
-			outfiles.append(out)
-			self.dump_all_databases(out)
+			outfile = os.path.join(self.output_dir, filename)
+			self.dump_database(database, outfile)
 
-		# compress the sql file if configured
-		if self.compress:
-			for f in outfiles:
-				self.compress_file(f, f + ".tar.bz2")
-				os.remove(f)
 
 if __name__ == '__main__':
-	from optparse import OptionParser
+	parser = ArgumentParser(description='MySQL dump helper')
+	parser.add_argument('-c', '--defaults-file', required=True, help='Path to a mysql defaults file')
+	parser.add_argument('-o', '--output-dir', default='/var/backups/mysql', help='Output directory (default: %(default)s)')
+	parser.add_argument('-f', '--format', default="%(name)s.sql.gz", help="Python format string for the output file names (default: %(default)s)")
+	parser.add_argument('-v', '--verbose', action='count', help='Increase verbosity', default=2)
 
-	parser = OptionParser()
-	parser.add_option("-s", "--server")
-	parser.add_option("-u", "--user")
-	parser.add_option("-p", "--password")
-	parser.add_option("-d", "--directory")
+	args = parser.parse_args()
 
-	(options, args) = parser.parse_args()
+	logging.basicConfig(level=logging.FATAL - (10 * args.verbose), format='%(asctime)s %(levelname)-7s %(message)s')
 
-	if not options.server or not options.user or not options.password or not options.directory:
-		parser.print_help()
-		sys.exit(1)
-
-	dumper = DatabaseDumper(options.server, options.user, options.password, options.directory, True, True, ' '.join(args))
+	dumper = DatabaseDumper(args.defaults_file, args.output_dir, args.format)
 	dumper.execute()
